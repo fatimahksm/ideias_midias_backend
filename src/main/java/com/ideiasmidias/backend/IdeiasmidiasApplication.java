b3e7fc98d62b86ea701ab3c901c1764316c852ad
Fatima Kassem <kassemf96@gmail.com>
2026-03-19 22:40:37 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/ideiasmidias/backend/IdeiasmidiasApplication.java
@@ -0,0 +1,13 @@
+package com.ideiasmidias.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeiasmidiasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeiasmidiasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ideiasmidias/backend/IdeiasmidiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideiasmidias.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeiasmidiasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
